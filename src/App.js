b0465f6bfc59e61a12dd07a63db10de1f1926434
cod2048 <cod2048@gmail.com>
2023-07-09 11:22:12 +0900

    사용자 위치정보 가져오기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 
 
@@ -6,8 +7,20 @@ import './App.css';
 // 3. 5개의 버튼이 있음(현재위치와 타도시)
 // 4. 도시버튼 클릭 시마다 도시별 날씨가 보임
 // 5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나옴
+// 6. 로딩스피너도 맹글기
 
 function App() {
+  
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log("현재위치", lat, lon);
+    }); // 사용자 위치 확인
+  }
+  useEffect(()=>{
+    getCurrentLocation()
+  },[]);
   return (
     <div>
       
