cf42e82c33032956e02b979fbc72d095310cd473
cod2048 <cod2048@gmail.com>
2023-07-10 12:12:42 +0900

    날씨API 연결

--- src/App.js
+++ src/App.js
@@ -15,9 +15,17 @@ function App() {
     navigator.geolocation.getCurrentPosition((position)=>{
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
-      console.log("현재위치", lat, lon);
+      getWeatherByCurrentLocation(lat,lon);
     }); // 사용자 위치 확인
   }
+
+  const getWeatherByCurrentLocation = async (lat, lon) =>{
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=82846df8ec305de4ad82b0cdc6c7e2af`
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data", data);
+  }
+
   useEffect(()=>{
     getCurrentLocation()
   },[]);
